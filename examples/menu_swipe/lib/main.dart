28e7d1a047b4879da86cba6a735679eac3462965
Yann-Cyril PELUD <ypelud@gmail.com>
2018-02-15 15:56:52 +0100

    Improve examples swipe

--- examples/menu_swipe/lib/main.dart
+++ examples/menu_swipe/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:menu_swipe_helpers/menu_swipe_helpers.dart';
 
+const String _kAsset0 = 'avatars/yann.jpg';
+const String _kGalleryAssetsPackage = 'flutter_plugins_assets';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -31,9 +34,11 @@ class MyApp extends StatelessWidget {
   Widget _userAccountDrawer(BuildContext context) => new UserAccountsDrawerHeader(
     accountName: new Text("Yann-Cyril Pelud"),
     accountEmail: new Text("yann@fidelisa.com"),
-    currentAccountPicture: new CircleAvatar(
-      backgroundColor: Theme.of(context).primaryColor,
-      child: new Text("YP"),
+    currentAccountPicture: const CircleAvatar(
+      backgroundImage: const AssetImage(
+        _kAsset0,
+        package: _kGalleryAssetsPackage,
+      ),
     ),
     margin: EdgeInsets.zero,
   );
@@ -46,7 +51,7 @@ class FirstPage extends StatefulWidget implements FdlsDrawerPage {
   _FirstPage createState() => new _FirstPage();
 
   @override
-  Icon get icon => const Icon(Icons.first_page);
+  Icon get icon => const Icon(Icons.home);
 
   @override
   String get subtitle => null;
@@ -57,6 +62,32 @@ class FirstPage extends StatefulWidget implements FdlsDrawerPage {
 
 class _FirstPage extends State<FirstPage> with FdlsDrawerPageState {
 
+  @override
+  Widget buildBody() {
+    var _style = Theme.of(context).textTheme.subhead;
+    return new Container(
+      decoration: new BoxDecoration(color: Colors.green),
+      child: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: new Text(
+              '''
+Very basic page, go to the seconde page to see more
+              ''',
+              style: _style,
+              softWrap: true),
+            ),
+          ],
+        ),
+      )
+    );
+
+  } 
+
+
 }
 
 class SecondPage extends StatefulWidget implements FdlsDrawerPage {
@@ -66,40 +97,78 @@ class SecondPage extends StatefulWidget implements FdlsDrawerPage {
   _SecondPage createState() => new _SecondPage();
 
   @override
-  Icon get icon => const Icon(Icons.last_page);
+  Icon get icon => const Icon(Icons.settings);
 
   @override
   String get subtitle => null;
 
   @override
-  String get title => "Second page";
+  String get title => "Seconde Page";
 }
 
 class _SecondPage extends State<SecondPage> with FdlsDrawerPageState {
 
   @override
-  Widget buildBody() => new Container(
+  Widget buildBody() {
+    var _style = Theme.of(context).textTheme.subhead;
+    return new Container(
       decoration: new BoxDecoration(color: Colors.red),
       child: new Center(
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            new Text("Demo",style: _style),
             new Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: new Text(title,
-                style: Theme.of(context).textTheme.subhead,
-              ),
+              padding: const EdgeInsets.all(32.0),
+              child: new Text(
+              '''
+Action in app bar
+Persistent Footer Button
+Floating button
+              ''',
+              style: _style,
+              softWrap: true),
             ),
           ],
         ),
       )
   );
 
+  } 
 
   List<Widget> buildActions() => [
     new IconButton(
       icon: new Icon(Icons.edit),
-      onPressed: () => {},
+      onPressed: () => {
+        //TODO not implemented
+      },
     ),
   ];
+
+  @override
+  List<Widget> buildPresistentFooterButtons() {
+    return [
+      new ButtonBar(
+        alignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new FlatButton(
+              onPressed: () => {
+                //TODO not implemented
+              },
+              child: new Text("BUTTON")),
+        ],
+      )
+    ];
+  }
+
+  @override
+  Widget buildFloatingButton() {
+    return new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: () => {
+          //TODO not implemented          
+        });
+  }
+
 }
